c7017dd2d5063b347ee50a60ac2589e850ec3a5d
omarisrafil <omar.israfil@bil.omu.edu.tr>
2017-07-03 16:09:19 +0300

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   #get 'users/new'
+  resources :users
 
   get '/signup', to: 'users#new'
 
@@ -9,6 +10,8 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about'
 
   get '/contact', to: 'static_pages#contact'
+
+  post '/signup',  to: 'users#create'
 
   root 'static_pages#home'
 
